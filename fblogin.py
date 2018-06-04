a5425eb05171dce2a95dc7485c6f45b5e405f9e0
Aniket_Dukare <34681686+ob-liv-ous@users.noreply.github.com>
2018-06-04 21:15:32 +0530

    Update fblogin.py

--- fblogin.py
+++ fblogin.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-browser=webdriver.Chrome('/home/aniket/Documents/chromedriver')
+browser=webdriver.Chrome('location of your chrome driver')
 browser.get('https://www.facebook.com/')
 assert "Facebook" in browser.title
-ele= browser.find_element_by_id("email")
-ele.send_keys('your email ID')
-ele=browser.find_element_by_id("pass")
-ele.send_keys('facebook password')
-ele.send_keys(Keys.RETURN)
+elem= browser.find_element_by_id("email")
+elem.send_keys('your email ID')
+elem=browser.find_element_by_id("pass")
+elem.send_keys('your facebook password')
+elem.send_keys(Keys.RETURN)
 
 
 
